75c376c7805a9a1c1dce981c83df5296154e4691
Andreffelipe <felipe.andre35@yahoo.com.br>
2020-07-25 18:29:38 -0300

    api completa

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,8 +1,14 @@
 import { Router } from 'express';
-
+import multer from 'multer';
 import CreateUserService from '../service/CreateUserService';
+import ensureAuthenticate from '../middlewares/ensureAuthenticated';
+
+import uploadConfig from '../config/upload';
+
+import UpdateAvatarUserService from '../service/UploadUserAvatarService';
 
 const userRouter = Router();
+const upload = multer(uploadConfig);
 
 userRouter.post('/', async (request, response) => {
     try {
@@ -17,5 +23,24 @@ userRouter.post('/', async (request, response) => {
         return response.status(400).json({ error: err.message });
     }
 });
+
+userRouter.patch(
+    '/avatar',
+    ensureAuthenticate,
+    upload.single('avatar'),
+    async (request, response) => {
+        try {
+            const updateUserAvatar = new UpdateAvatarUserService();
+            const user = await updateUserAvatar.execute({
+                user_id: request.user.id,
+                avatarFileName: request.file.filename,
+            });
+            delete user.password;
+            return response.json(user);
+        } catch (err) {
+            return response.status(400).json({ error: err.message });
+        }
+    }
+);
 
 export default userRouter;
